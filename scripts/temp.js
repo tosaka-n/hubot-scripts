b48dbf05e76562af2a472aa05c7fe995d3e577ac
tosaka-n <31643545+tosaka-n@users.noreply.github.com>
2020-11-25 10:57:02 +0900

    if snowfall added (#2)

--- scripts/temp.js
+++ scripts/temp.js
@@ -1,5 +1,8 @@
 const fetch = require("node-fetch");
 const JSDOM = require('jsdom').JSDOM;
+const values = ["time", "temp", "precipitation", "direction", "speed", "sunshine_hours", "humidity", "barometric_pressure"];
+const val_ja = ["時刻", "気温", "降水量", "風向", "風速", "日照時間", "湿度", "気圧"];
+const units = ["時", "℃", "mm", "", "m/s", "h", "%", "hPa"];
 async function repro(url) {
   const response = await fetch(url);
   const text = await response.text();
@@ -11,13 +14,15 @@ function arr2obj(arr, key) {
 }
 async function getTemps() {
   const document = await repro('https://www.jma.go.jp/jp/amedas_h/today-44132.html');
-  const values = ["time", "temp", "precipitation", "direction", "speed", "sunshine_hours", "snow_depth", "humidity", "barometric_pressure"];
-  const val_ja = ["時刻", "気温", "降水量", "風向", "風速", "日照時間", "湿度", "気圧"];
-  const units = ["時", "℃", "mm", "", "m/s", "h", "%", "hPa"];
-  let time = new Date().getHours()  + 2 + 9;
+  let time = new Date().getHours() + 2 + 9;
   if (new Date().getMinutes() < 10) { time--; }
   const result = Array.from(document.querySelectorAll(`#tbl_list > tbody > tr:nth-child(${time}) > td`)).map(v => v.innerHTML);
-  const maps = result.map((p, i) => ({value: values[i], amount: p, unit: units[i], ja: val_ja[i] }), {});
+  if (result.length > 8) {
+    values.splice(5, 0, "snowfall");
+    val_ja.splice(5, 0, "積雪深");
+    units.splice(5, 0, "mm");
+  }
+  const maps = result.map((p, i) => ({ value: values[i], amount: p, unit: units[i], ja: val_ja[i] }), {});
   return maps
 }
 
